348225cdb544263d9ff8abb8afbbd8b1d0aab930
rebecavasco-prog <rebeca.vasco@estudante.ifb.edu.br>
2025-11-22 16:27:17 -0300

    Add Login component with form elements

---
+++ components/Login.jsx
@@ -0,0 +1,30 @@
+import "./Login.css";
+
+import Label from "./Label.jsx";
+import LabelTitle from "./LabelTitle.jsx";
+import Button from "./Button.jsx";
+import InputText from "./InputText.jsx";
+import InputPassword from "./InputPassword.jsx";
+
+export default function Login() {
+  return (
+    <div className="login-container">
+      <form className="login-form">
+
+        <LabelTitle texto="Login" />
+
+        <div className="form-group">
+          <Label texto="Usuário:" />
+          <InputText placeholder="user..." />
+        </div>
+
+        <div className="form-group">
+          <Label texto="Senha:" />
+          <InputPassword placeholder="password..." />
+        </div>
+
+        <Button valor="Logar" />
+      </form>
+    </div>
+  );
+}
